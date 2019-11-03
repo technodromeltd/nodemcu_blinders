366b2fa4f769ca36fd7f680a19597dc36e1b8960
Eero Junttila <junttila.eero@gmail.com>
2019-11-03 13:55:11 +0200

    ignore

--- settings.h
+++ settings.h
@@ -4,6 +4,6 @@ const char* ssid = "xxx"; //your WiFi Name
 const char* password = "xxx";  //Your Wifi Password
 
 IPAddress ip(192, 168, xxx, xxx);
-IPAddress gateway(192, 168, xxx, xxx);
+IPAddress gateway(192, 168, 0, xxx);
 IPAddress subnet(255, 255, 255, 0);
 String hostName = "blinders-livingroom"
